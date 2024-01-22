d2a54eef3585377d2c4a3c7185cce330e1fdf9c8
Giuseppe Raiolo <giusepperaiolo@hotmail.com>
2024-01-22 21:19:16 +0100

    add ownwer

--- angular_volume/src/app/features/components/chat/components/user-list/user-list.component.ts
+++ angular_volume/src/app/features/components/chat/components/user-list/user-list.component.ts
@@ -54,7 +54,6 @@ export class UserListComponent implements OnInit, OnDestroy{
 						const id = user.user.id;
 						const blockList = user.user.blockedBy;
 						const banOrKick = user.status;
-						console.log('banOrKick:', banOrKick);
 						let isBlock = false;
 						for (let blockUser of blockList) {
 							if (blockUser.blocker.username === this.user.username) {
@@ -73,8 +72,17 @@ export class UserListComponent implements OnInit, OnDestroy{
 						}
 						else {
 							this.myRole = role;
-							console.log('myRole:', this.myRole);
 						}
+					}
+					if (this.players.length === 0) {
+						this.players.push({ 
+							id: '',
+							name: 'No users in this channel',
+							showMenu: false,
+							role: '',
+							isBlock: false,
+							banOrKick: ''
+						});
 					}
 				},	
 			})
